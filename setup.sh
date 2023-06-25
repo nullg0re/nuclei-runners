226fb376042f769e8cfefc739e0d83a7b86d4480
Tony Gore <tgore@secureworks.com>
2023-06-25 09:16:16 -0500

    a

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # install dependecies
-sudo apt install curl wget git zip dnsutils net-tools
+sudo apt install -y curl wget git zip dnsutils net-tools
 # install nuclei and nuclei templates
 wget https://github.com/projectdiscovery/nuclei/releases/download/v2.9.6/nuclei_2.9.6_linux_amd64.zip
 unzip nuclei_2.9.6_linux_amd64.zip
@@ -9,12 +9,7 @@ nuclei -ut
 # git clone runners
 git clone https://github.com/nullg0re/nuclei-runners
 # make xfiles and run directories
-cd nuclei-runners
-mkdir xfiles
-for i in $(seq 1 25);
-do
-    mkdir xfiles/run$i
-done
+mkdir nuclei-runners/xfiles
 # make output directory
 cd ~
 mkdir output
