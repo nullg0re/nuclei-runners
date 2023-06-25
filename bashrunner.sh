5310d5ac63a401ed1b55c51045d908bf0cedbd53
Tony Gore <tgore@secureworks.com>
2023-06-25 10:15:53 -0500

    d

--- bashrunner.sh
+++ bashrunner.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #for i in $(seq 1 103);
 server=`hostname`
-curl -X POST -H 'Content-type: application/json' --data "{""text"":""[+] @U03JU7F3SCX STARTING RUN::: Server: $server""}" https://hooks.slack.com/services/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
+curl -X POST -H 'Content-type: application/json' --data "{""text"":""[+] @U03JU7F3SCX STARTING RUN::: Server: $server""}" $(cat webhook.txt)
 for i in $(seq 1 25);
 do
     bash run$i.sh &
 done
-curl -X POST -H 'Content-type: application/json' --data "{""text"":""[+] @U03JU7F3SCX FINISHED RUN - KILL SERVER::: Server: $server""}" https://hooks.slack.com/services/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
+curl -X POST -H 'Content-type: application/json' --data "{""text"":""[+] @U03JU7F3SCX FINISHED RUN - KILL SERVER::: Server: $server""}" $(cat webhook.txt)
--- run13.sh
+++ run13.sh
@@ -7,6 +7,6 @@ do
     echo "EXECUTING FILE: $run/$file"
     count=`wc -l *-nuclei.txt`
     nuclei -l $run/$file -t ~/nuclei-templates/http/exposures -o ~/output/$x-$file-nuclei.txt
-    curl -X POST -H 'Content-type: application/json' --data "{""text"":""[+] Server: $server | Runner: $x | File: $file | Secrets Count: $count""}" $(cat webhook.txt)
+    curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"[+] Server: $server | Runner: $x | File: $file | Secrets Count: $count\"}" $(cat webhook.txt)
     echo "FILE EXECUTED: $run/$file"
 done
