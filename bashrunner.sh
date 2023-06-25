5704d532ed32b2d713c5d9ab7acfede11e932934
Tony Gore <tgore@secureworks.com>
2023-06-25 10:19:37 -0500

    e

--- bashrunner.sh
+++ bashrunner.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #for i in $(seq 1 103);
 server=`hostname`
-curl -X POST -H 'Content-type: application/json' --data "{""text"":""[+] @U03JU7F3SCX STARTING RUN::: Server: $server""}" $(cat webhook.txt)
+curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"[+] @U03JU7F3SCX STARTING RUN::: Server: $server\"}" $(cat webhook.txt)
 for i in $(seq 1 25);
 do
     bash run$i.sh &
 done
-curl -X POST -H 'Content-type: application/json' --data "{""text"":""[+] @U03JU7F3SCX FINISHED RUN - KILL SERVER::: Server: $server""}" $(cat webhook.txt)
+curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"[+] @U03JU7F3SCX FINISHED RUN - KILL SERVER::: Server: $server\"}" $(cat webhook.txt)
