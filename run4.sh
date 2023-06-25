01bd08db191aa8f32a6b9700d99c1cae46947c56
Tony Gore <tgore@secureworks.com>
2023-06-25 10:59:18 -0500

    g

--- run4.sh
+++ run4.sh
@@ -5,7 +5,7 @@ server=`hostname`
 for file in $(ls $run);
 do
     echo "EXECUTING FILE: $run/$file"
-    count=`wc -l /home/tgore/output/*-nuclei.txt`
+    count=`wc -l ~/output/*-nuclei.txt`
     nuclei -l $run/$file -t ~/nuclei-templates/http/exposures -o ~/output/$x-$file-nuclei.txt
     curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"[+] Server: $server | Runner: $x | File: $file | Secrets Count: $count\"}" $(cat webhook.txt)
     echo "FILE EXECUTED: $run/$file"
